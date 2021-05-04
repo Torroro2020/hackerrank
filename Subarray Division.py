404b146b7adf211199e35998cb842389d019c775
Torroro2020 <tomeartur@yandex.ru>
2021-05-04 21:02:19 +0300

    деление шоколадки на части

--- Subarray Division.py
+++ Subarray Division.py
@@ -7,34 +7,40 @@ import re
 import sys
 
 
+# Complete the birthday function below.
 def birthday(s, d, m):
-    cnt = 0
-    for i in range(n-1):
-        sum_num = 0
-        print('До внутр цикла: ', sum_num)
-        for y in range(m):
-            sum_num += s[i+y]
-            print('После увеличения значения: ', sum_num)
-        if sum_num == d:
-            cnt += 1
-    return cnt
+    f = 0
+    if m == 1 and sum(s) == d:
+        return 1
+
+    for i in range((n - m) + 1):
+        subarray, a = 0, 0
+
+        while subarray != m:
+            a += s[i]
+            i += 1
+            subarray += 1
+
+        if a == d:
+            f += 1
+    return f
 
 
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
 
+    n = int(input().strip())
 
+    s = list(map(int, input().rstrip().split()))
 
+    dm = input().rstrip().split()
 
+    d = int(dm[0])
 
-n = int(input().strip())
+    m = int(dm[1])
 
-s = list(map(int, input().rstrip().split()))
+    result = birthday(s, d, m)
 
-dm = input().rstrip().split()
+    fptr.write(str(result) + '\n')
 
-d = int(dm[0])
-
-m = int(dm[1])
-
-result = birthday(s, d, m)
-
-print(result)
+    fptr.close()
